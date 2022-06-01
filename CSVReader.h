c9aaaa7f6c696857983fa7bc922de23a0cbd4d6f
Wisaacj <wisaacj@live.co.uk>
2022-06-01 13:49:57 +0100

    Complete: Read .csv into matrix

--- CSVReader.h
+++ CSVReader.h
@@ -1,19 +1,32 @@
 //
-// Created by Will on 31/05/2022.
+// Created by Will on 01/06/2022.
 //
 
 #ifndef MNIST_NEURAL_NET_CSVREADER_H
 #define MNIST_NEURAL_NET_CSVREADER_H
-#include <iostream>
 
-class CSVReader {
-public:
-    CSVReader(); // Default constructor
-    virtual ~CSVReader(); // De-constructor
+#include <Eigen/Dense>
+#include <fstream>
+#include <vector>
 
-    template<typename M>
-    M load_csv(const std::string & path);
-};
+using namespace Eigen;
 
+template<typename M>
+M load_csv (const std::string & path) {
+    std::ifstream indata;
+    indata.open(path);
+    std::string line;
+    std::vector<double> values;
+    unsigned int rows = 0;
+    while (std::getline(indata, line)) {
+        std::stringstream lineStream(line);
+        std::string cell;
+        while (std::getline(lineStream, cell, ',')) {
+            values.push_back(std::stod(cell));
+        }
+        ++rows;
+    }
+    return Map<const Matrix<typename M::Scalar, M::RowsAtCompileTime, M::ColsAtCompileTime, RowMajor>>(values.data(), rows, values.size()/rows);
+}
 
 #endif //MNIST_NEURAL_NET_CSVREADER_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 #include <Eigen/Dense>
+#include "CSVReader.h"
+
+using namespace std;
+using namespace Eigen;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Testing load_csv method
+    MatrixXd test = load_csv<MatrixXd>(R"(C:\Users\Will\OneDrive\Projects\C++\mnist-neural-net\data\mnist_test.csv)");
+    // Printing out the first 10 rows of the test dataset
+    cout << test(Eigen::seqN(0,1,1), Eigen::all) << endl;
+    
     return 0;
 }
